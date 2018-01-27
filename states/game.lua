a01305c202ba38507cc3052ee3add66635b86e1e
Pascal Le Merrer <pascal.lemerrer@orange.com>
2018-01-27 11:41:01 +0100

    Add list of bubbles to game

--- states/game.lua
+++ states/game.lua
@@ -2,18 +2,24 @@ Game = {}
 
 require "gameobjects.bubble"
 
+
+
 function Game:init()
-    self.sentence = sentences[level]
+  self.sentence = sentences[level]
+  self.bubbles = {}
 end
 
 function Game:enter(previous) -- runs every time the state is entered
-  self.bubble = Bubble(WIN_WIDTH/2, WIN_HEIGHT/2, 20, "La p")
+  self:createBubble()
 end
 
 function Game:update(dt) -- runs every frame
 end
 
 function Game:draw()
-  love.graphics.print(self.sentence, 10, 10)
-  self.bubble:draw()
+  -- love.graphics.print(self.sentence, 10, 10)
+  for index, bubble in ipairs(self.bubbles) do
+    bubble:draw()
+  end
 end
+
