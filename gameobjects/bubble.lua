ea14863d5f7834af0d9e5e8a558ff6ce27076ec6
Pascal Le Merrer <pascal.lemerrer@orange.com>
2018-01-27 11:07:50 +0100

    Add bubble game object

---
+++ gameobjects/bubble.lua
@@ -0,0 +1,21 @@
+Class = require 'hump.class'
+
+Bubble = Class{}
+
+function Bubble:init(x, y, radius, label)
+  self.label = label
+  self.radius = radius
+  self.x = x
+  self.y = y
+  self.image = love.graphics.newImage("resources/images/bulle.png") 
+  self.rotation = 0
+  self.scale = 1
+end
+
+function Bubble:__tostring()
+  return "bubble [ radius = " .. self.radius .. ", label = \"" .. self.label .. "\" ]"
+end
+
+function Bubble:draw()
+  love.graphics.draw(self.image, self.x, self.y, self.rotation, self.scale, self.scale)
+end
--- states/game.lua
+++ states/game.lua
@@ -1,10 +1,13 @@
 Game = {}
 
+require "gameobjects.bubble"
+
 function Game:init()
     self.sentence = sentences[level]
 end
 
 function Game:enter(previous) -- runs every time the state is entered
+  self.bubble = Bubble(WIN_WIDTH/2, WIN_HEIGHT/2, 20, "La p")
 end
 
 function Game:update(dt) -- runs every frame
@@ -12,4 +15,5 @@ end
 
 function Game:draw()
   love.graphics.print(self.sentence, 10, 10)
+  self.bubble:draw()
 end
